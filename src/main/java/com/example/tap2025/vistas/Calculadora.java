4351bdf4ba70cc7c4872f2675cfb65c3f8a90a24
Cherrisita <juangelrt21@gmail.com>
2025-02-16 08:10:25 -0600

    Text validations

--- src/main/java/com/example/tap2025/vistas/Calculadora.java
+++ src/main/java/com/example/tap2025/vistas/Calculadora.java
@@ -10,6 +10,7 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class Calculadora extends Stage
@@ -20,6 +21,7 @@ public class Calculadora extends Stage
         private Button [][] keypad;
         private Scene scene;
         private String [] button_text;
+        private String operator;
 
         public void create_ui() {
             button_text = new String[]{"7", "8", "9", "÷", "4", "5", "6", "x", "1", "2", "3"
@@ -72,27 +74,70 @@ public class Calculadora extends Stage
 
         public void pressed_button(String value)
         {
-            switch (value) {
+            switch (value)
+            {
                 case "=":
                     result();
                     break;
-                case "+":
-                    break;
-                case "-":
-                    break;
-                case "x":
-                    break;
-                case "÷":
-                    break;
                 default:
-                    text_field.appendText(value);
+                    if(text_field.getText().equals("0") && !value.equals("."))
+                        text_field.setText(value);
+                    else
+                        text_field.appendText(value);
+                    set_simbol(value);
                     break;
             }
         }
 
-        public void result()
+        public void set_simbol(String simbol)
         {
             System.out.println("Me gusta el pene");
+        }
+
+        public void result()
+        {
+            String text = text_field.getText();
+            System.out.println(operator);
+            System.out.println(text);
+            String [] operation = text.split(operator);
+            double part1 = Double.parseDouble(operation[0]);
+            double part2 = Double.parseDouble(operation[1]);
+            double result = 0;
+
+            switch (operator)
+            {
+                case "\\+":
+                    result = part1 + part2;
+                    break;
+                case "-":
+                    result = part1 - part2;
+                    break;
+                case "x":
+                    result = part1 * part2;
+                    break;
+                case "÷":
+                    result = part1 / part2;
+                    break;
+            }
+            if (result % 1 == 0)
+            {
+                long result_long = Math.round(result);
+                text_field.setText(result_long + "");
+            }
+            else
+            {
+                String result_string = String.valueOf(result);
+                String decimal_format = "#.";
+                int excess = result_string.length() - 14;
+                System.out.println("Tamaño del resultado = " + result_string.length());
+                for (int i = 0; i < 14 - excess; i++)
+                {
+                    decimal_format += "#";
+                }
+                System.out.println("El decimal format quedó como: " + decimal_format);
+                DecimalFormat df = new DecimalFormat(decimal_format);
+                text_field.setText(String.valueOf(df.format(result)));
+            }
         }
 
         public Calculadora()
