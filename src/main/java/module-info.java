2d466617588cdf9effe4f869de26838c64c65187
Cherrisita <juangelrt21@gmail.com>
2025-02-19 12:33:23 -0600

    Conexion a MySQL

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,10 @@
-module com.example.tap2025 {
+module com.example.tap2025
+{
     requires javafx.controls;
     requires javafx.fxml;
     requires org.kordamp.bootstrapfx.core;
+    requires mysql.connector.j;
+    requires java.sql;
 
     opens com.example.tap2025 to javafx.fxml;
     exports com.example.tap2025;
