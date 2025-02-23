c2b88df83e12184173fc0fa4a29d9ce4d78af910
Cherrisita <juangelrt21@gmail.com>
2025-02-22 21:26:02 -0600

    Interfaz Rompecabezas

--- src/main/java/com/example/tap2025/vistas/Rompecabezas.java
+++ src/main/java/com/example/tap2025/vistas/Rompecabezas.java
@@ -1,9 +1,12 @@
 package com.example.tap2025.vistas;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -11,48 +14,69 @@ public class Rompecabezas extends Stage
 {
     private Scene scene;
     private VBox vbox;
-    private HBox hbox_menu;
-    private HBox hbox_interface;
-    private Button reset;
-    private Button change_difficulty;
-    private Label best_time;
-    private Label timer;
+    private HBox hbox_menu, hbox_menu_left, hbox_menu_right, hbox_interface;
+    private Button reset, change_difficulty;
+    private Label best_time, timer;
 
     public void create_ui()
     {
         reset = new Button("Reiniciar");
         change_difficulty = new Button("Cambiar dificultad");
         change_difficulty.setOnAction(e -> create_menu_selection());
+        hbox_menu_left = new HBox(reset, change_difficulty);
+        hbox_menu_left.setSpacing(10);
+        hbox_menu_left.setAlignment(Pos.CENTER_LEFT);
+        HBox.setHgrow(hbox_menu_left, Priority.ALWAYS);
+
         best_time = new Label("Mejor tiempo: --:--:--");
         timer = new Label("Contador: --:--:--");
-        hbox_menu = new HBox(reset, change_difficulty, best_time, timer);
+        hbox_menu_right = new HBox(best_time, timer);
+        hbox_menu_right.setSpacing(10);
+        hbox_menu_right.setAlignment(Pos.CENTER_RIGHT);
+        HBox.setHgrow(hbox_menu_right, Priority.ALWAYS);
+
+        hbox_menu = new HBox(hbox_menu_left, hbox_menu_right);
+        hbox_menu.setPadding(new Insets(10, 10, 20, 10));
+        hbox_menu.setSpacing(10);
         hbox_interface = new HBox();
         vbox = new VBox(hbox_menu, hbox_interface);
         scene = new Scene(vbox);
+        scene.getStylesheets().add(getClass().getResource("/styles/rompecabezas.css").toExternalForm());
         setTitle("Rompecabezas");
         show();
-        this.setMaximized(true);
     }
 
     public void create_menu_selection()
     {
         Label difficulty = new Label("Dificultad");
-        Label best_time = new Label("Mejor tiempo");
-        HBox title = new HBox(difficulty, best_time);
         Button easy  = new Button("Fácil");
-        Label high_score_easy = new Label("--:--:--");
-        HBox hbox_easy = new HBox(easy, high_score_easy);
+        easy.setPrefSize(120, 20);
         Button medium = new Button("Normal");
-        Label high_score_medium = new Label("--:--:--");
-        HBox hbox_medium = new HBox(medium, high_score_medium);
+        medium.setPrefSize(120, 20);
         Button hard = new Button("Difícil");
-        Label high_score_hard = new Label("--:--:--");
-        HBox hbox_hard = new HBox(hard, high_score_hard);
-        VBox vbox = new VBox(title, hbox_easy, hbox_medium, hbox_hard);
-        Scene menu_scene = new Scene(vbox, 500, 200);
+        hard.setPrefSize(120, 20);
+        VBox vbox_buttons = new VBox(difficulty, easy, medium, hard);
+        vbox_buttons.setSpacing(10);
+        vbox_buttons.setAlignment(Pos.CENTER_LEFT);
+
+        Label best_time = new Label("Mejor tiempo");
+        Label high_score_easy = new Label("21:09:24");
+        Label high_score_medium = new Label("21:09:24");
+        Label high_score_hard = new Label("21:09:24");
+        VBox vbox_labels = new VBox(best_time, high_score_easy, high_score_medium, high_score_hard);
+        vbox_labels.setSpacing(20);
+        vbox_labels.setAlignment(Pos.TOP_RIGHT);
+
+        HBox hbox = new HBox(vbox_buttons, vbox_labels);
+        hbox.setSpacing(30);
+        hbox.setAlignment(Pos.CENTER);
+        hbox.setPadding(new Insets(10, 10, 10, 10));
+        Scene menu_scene = new Scene(hbox);
+        menu_scene.getStylesheets().add(getClass().getResource("/styles/rompecabezas.css").toExternalForm());
         Stage menu_stage = new Stage();
         menu_stage.setScene(menu_scene);
         menu_stage.setTitle("Selección de dificultad");
+        menu_stage.setResizable(false);
         menu_stage.show();
     }
 
@@ -60,6 +84,5 @@ public class Rompecabezas extends Stage
     {
         create_ui();
         setScene(scene);
-        create_menu_selection();
     }
 }
